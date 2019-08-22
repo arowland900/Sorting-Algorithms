20e018122867290abab9d6124c3421e0141c59d5
Alexander Rowland <arowland900@gmail.com>
2019-08-22 16:53:58 -0700

    clean up bubble sort function

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function bub(arr) {
     for (var i = 0; i < arr.length; i++) {
         for (var j = 0; j < arr.length - i; j++) {
             if (arr[j] > arr[j + 1]) {
-                var temporarilyStored = arr[j]
+                var tmp = arr[j]
                 arr[j] = arr[j + 1]
-                arr[j + 1] = temporarilyStored
+                arr[j + 1] = tmp
             }
         }
     }
