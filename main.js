0295a6e4407a19b649156f6e37256fbc3d0c6c0d
Alexander Rowland <arowland900@gmail.com>
2019-08-23 11:00:05 -0700

    clean up insertion sort funtion

--- main.js
+++ main.js
@@ -19,15 +19,12 @@ function insert(arr) {
     for (var i = 0; i < arr.length; i++) {
         for (var j = i - 1; j > -1; j--) {
             var tmp = arr[j + 1]
-            if (arr[j] > tmp) {
-                arr[j + 1] = arr[j]
-                arr[j] = tmp
-            } else {
-                break
-            }
+            if (arr[j] > tmp) arr[j + 1] = arr[j], arr[j] = tmp
+            else break
         }
     }
     return arr
 }
+
 
 console.log(insert([33, 6, 1, 4, 5]))
