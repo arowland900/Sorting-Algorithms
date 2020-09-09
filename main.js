6099dd3bac179c91850c7a996dc654dd131858ca
Alexander Rowland <arowland900@gmail.com>
2020-09-09 17:52:54 -0400

    add swap check

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 
 function bubbleSort(arr) {
     for (var i = 0; i < arr.length; i++) {
+        let swapped = false
         for (var j = 0; j < arr.length - i; j++) {
             if (arr[j] > arr[j + 1]) {
+                swapped = true
                 var tmp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = tmp
             }
         }
+        if(!swapped) return arr
     }
     return arr
 }
